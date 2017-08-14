25ee07cc42f7236daebe6905d462fac11c424243
Tim Broddin <tim@wannabes.be>
2017-08-14 15:33:18 +0200

    cleanup

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,147 +1,152 @@
-import config from '../config';
-import Auth from '../Auth';
+import config from "../config";
+import Auth from "../Auth";
 
 const auth = new Auth();
 
-const setCurrency = (currency) => {
-  return (dispatch) => {
+const setCurrency = currency => {
+  return dispatch => {
     dispatch({
-      type: 'SET_CURRENCY',
+      type: "SET_CURRENCY",
       currency
     });
 
     dispatch(fetchData());
     dispatch(saveUserData());
-  }
-}
+  };
+};
 
-const setData = (data) => {
+const setData = data => {
   return {
-    type: 'SET_DATA',
+    type: "SET_DATA",
     data
-  }
-}
+  };
+};
 
-
-const setDataLoading = (value) => {
+const setDataLoading = value => {
   return {
-    type: 'SET_DATA_LOADING',
+    type: "SET_DATA_LOADING",
     value
-  }
-}
-
+  };
+};
 
 const fetchData = () => {
   return (dispatch, getState) => {
-    const {currency} = getState();
+    const { currency } = getState();
 
     dispatch(setDataLoading(true));
-    fetch(`${config.api_base}ticker/?convert=${currency}`).then(res => res.json()).then((data) => {
-      dispatch(setData(data));
-      dispatch(fetchHistory());
-    }).catch(err => {
-      dispatch(setDataLoading(false));
-    })
-  }
-}
+    fetch(`${config.api_base}ticker/?convert=${currency}`)
+      .then(res => res.json())
+      .then(data => {
+        dispatch(setData(data));
+        dispatch(fetchHistory());
+      })
+      .catch(err => {
+        dispatch(setDataLoading(false));
+      });
+  };
+};
 
-const setHistoryLoading = (value) => {
+const setHistoryLoading = value => {
   return {
-    type: 'SET_HISTORY_LOADING',
+    type: "SET_HISTORY_LOADING",
     value
-  }
-}
+  };
+};
 
-const setHistory = (data) => {
+const setHistory = data => {
   return {
-    type: 'SET_HISTORY',
+    type: "SET_HISTORY",
     data
-  }
-}
+  };
+};
 
 const fetchHistory = () => {
   return (dispatch, getState) => {
     const { currency, coins } = getState();
     dispatch(setHistoryLoading(true));
-    if(coins && coins.length) {
-      fetch(`${config.api_base}previous?coins=${coins.map((c) => c.id).join(',')}&convert=${currency}`).then(res => res.json()).then((data) => {
-        dispatch(setHistory(data));
-      }).catch((err) => {
-        dispatch(setHistoryLoading(false));
-      });
+    if (coins && coins.length) {
+      fetch(
+        `${config.api_base}previous?coins=${coins
+          .map(c => c.id)
+          .join(",")}&convert=${currency}`
+      )
+        .then(res => res.json())
+        .then(data => {
+          dispatch(setHistory(data));
+        })
+        .catch(err => {
+          dispatch(setHistoryLoading(false));
+        });
     }
-  }
-}
+  };
+};
 
 const addCoin = (coin, amount) => {
-  return (dispatch) => {
+  return dispatch => {
     dispatch({
-      type: 'ADD_COIN',
+      type: "ADD_COIN",
       coin,
       amount
     });
     dispatch(fetchHistory());
     dispatch(saveUserData());
-  }
+  };
+};
 
-}
-
-const removeCoin = (coin) => {
-  return (dispatch) => {
+const removeCoin = coin => {
+  return dispatch => {
     dispatch({
-      type: 'REMOVE_COIN',
-      coin,
+      type: "REMOVE_COIN",
+      coin
     });
     dispatch(saveUserData());
-  }
-}
+  };
+};
 
 const changeCoinAmount = (coin, amount) => {
-  return (dispatch) => {
+  return dispatch => {
     dispatch({
-      type: 'CHANGE_COIN_AMOUNT',
+      type: "CHANGE_COIN_AMOUNT",
       coin,
       amount
     });
     dispatch(saveUserData());
-  }
-}
+  };
+};
 
-
-const addToWatchList = (coin) => {
-  return (dispatch) => {
+const addToWatchList = coin => {
+  return dispatch => {
     dispatch({
-      type: 'ADD_TO_WATCHLIST',
-      coin,
-    });
-    dispatch(saveUserData());
-  }
-}
-
-const removeFromWatchList = (coin) => {
-  return (dispatch) => {
-    dispatch({
-      type: 'REMOVE_FROM_WATCHLIST',
+      type: "ADD_TO_WATCHLIST",
       coin
     });
     dispatch(saveUserData());
-  }
-}
+  };
+};
 
+const removeFromWatchList = coin => {
+  return dispatch => {
+    dispatch({
+      type: "REMOVE_FROM_WATCHLIST",
+      coin
+    });
+    dispatch(saveUserData());
+  };
+};
 
-const setScanning = (scanning) => {
+const setScanning = scanning => {
   return {
-    type: 'SET_SCANNING',
+    type: "SET_SCANNING",
     scanning
-  }
-}
+  };
+};
 
-const setPage = (page) => {
+const setPage = page => {
   return {
-    type: 'SET_PAGE',
+    type: "SET_PAGE",
     page
-  }
-}
+  };
+};
 
 /* Export */
 const exportData = () => {
@@ -152,133 +157,151 @@ const exportData = () => {
     dispatch(setExportLoading(true));
 
     let body = new URLSearchParams();
-    body.append('data', JSON.stringify(data));
+    body.append("data", JSON.stringify(data));
 
-    fetch(`${config.api_base}sync`, { method: 'POST', body }).then(res => res.json()).then((data) => {
-      dispatch(setExportCode(data.code));
-      dispatch(setExportLoading(false));
-    }).catch((err) => {
-      dispatch(setExportError(err));
-      dispatch(setExportLoading(false));
-    })
+    fetch(`${config.api_base}sync`, { method: "POST", body })
+      .then(res => res.json())
+      .then(data => {
+        dispatch(setExportCode(data.code));
+        dispatch(setExportLoading(false));
+      })
+      .catch(err => {
+        dispatch(setExportError(err));
+        dispatch(setExportLoading(false));
+      });
+  };
+};
 
-
-  }
-}
-
-const setExportLoading = (value) => {
+const setExportLoading = value => {
   return {
-    type: 'SET_EXPORT_LOADING',
+    type: "SET_EXPORT_LOADING",
     value
-  }
-}
+  };
+};
 
-const setExportError = (value) => {
+const setExportError = value => {
   return {
-    type: 'SET_EXPORT_ERROR',
+    type: "SET_EXPORT_ERROR",
     value
-  }
-}
+  };
+};
 
-const setExportCode = (value) => {
+const setExportCode = value => {
   return {
-    type: 'SET_EXPORT_CODE',
+    type: "SET_EXPORT_CODE",
     value
-  }
-}
+  };
+};
 
 /* Import */
-const doImport = (data) => {
-  return (dispatch) => {
+const doImport = data => {
+  return dispatch => {
     dispatch({
-      type: 'IMPORT',
+      type: "IMPORT",
       data
     });
     dispatch(fetchData());
     //dispatch(fetchHistory());
-  }
+  };
+};
 
-}
-
-const importData = (code) => {
+const importData = code => {
   return (dispatch, getState) => {
     dispatch(setImportLoading(true));
 
+    fetch(`${config.api_base}sync/${code}`)
+      .then(res => res.json())
+      .then(data => {
+        dispatch(doImport(data));
+        dispatch(setImportLoading(false));
+        dispatch(setImportSuccess(true));
+      })
+      .catch(err => {
+        dispatch(setImportError(err));
+        dispatch(setImportLoading(false));
+        dispatch(setImportSuccess(false));
+      });
+  };
+};
 
-    fetch(`${config.api_base}sync/${code}`).then(res => res.json()).then((data) => {
-      dispatch(doImport(data));
-      dispatch(setImportLoading(false));
-      dispatch(setImportSuccess(true));
-    }).catch((err) => {
-      dispatch(setImportError(err));
-      dispatch(setImportLoading(false));
-      dispatch(setImportSuccess(false));
-
-    })
-
-
-  }
-}
-
-const setImportLoading = (value) => {
+const setImportLoading = value => {
   return {
-    type: 'SET_IMPORT_LOADING',
+    type: "SET_IMPORT_LOADING",
     value
-  }
-}
+  };
+};
 
-const setImportSuccess = (value) => {
+const setImportSuccess = value => {
   return {
-    type: 'SET_IMPORT_SUCCESS',
+    type: "SET_IMPORT_SUCCESS",
     value
-  }
-}
+  };
+};
 
-const setImportError = (value) => {
+const setImportError = value => {
   return {
-    type: 'SET_IMPORT_ERROR',
+    type: "SET_IMPORT_ERROR",
     value
-  }
-}
+  };
+};
 
 /* Load / save data */
 
 const loadUserData = () => {
   return (dispatch, getState) => {
-    if(auth.isAuthenticated()) {
-        const { getAccessToken } = auth;
-        const headers = new Headers();
-        headers.append('Authorization', `Bearer ${getAccessToken()}`);
+    if (auth.isAuthenticated()) {
+      const { getAccessToken } = auth;
+      const headers = new Headers();
+      headers.append("Authorization", `Bearer ${getAccessToken()}`);
 
-        fetch(`${config.api_base}secure/load`, { headers }).then(res => res.json()).then((data) => {
-          if(data) {
+      fetch(`${config.api_base}secure/load`, { headers })
+        .then(res => res.json())
+        .then(data => {
+          if (data) {
             dispatch(doImport(data));
           } else {
             // no data, create user Profile
             dispatch(saveUserData());
           }
-        });
-    }
-  }
-}
+        }).catch(err => {});
 
+    }
+  };
+};
 
 const saveUserData = () => {
   return (dispatch, getState) => {
-    if(auth.isAuthenticated()) {
-        const { getAccessToken } = auth;
-        const { coins, wishlist, currency } = getState();
+    if (auth.isAuthenticated()) {
+      const { getAccessToken } = auth;
+      const { coins, wishlist, currency } = getState();
 
-        const headers = new Headers();
-        headers.append('Authorization', `Bearer ${getAccessToken()}`);
+      const headers = new Headers();
+      headers.append("Authorization", `Bearer ${getAccessToken()}`);
 
-        const body = new URLSearchParams();
-        body.append('data', JSON.stringify( { coins, wishlist, currency }));
+      const body = new URLSearchParams();
+      body.append("data", JSON.stringify({ coins, wishlist, currency }));
 
-        fetch(`${config.api_base}secure/save`, { headers, body, method: 'POST' }).then(res => res.json()).then((data) => {
-        });
+      fetch(`${config.api_base}secure/save`, { headers, body, method: "POST" })
+        .then(res => res.json())
+        .then(data => {})
+        .catch(err => {});
     }
-  }
-}
+  };
+};
 
-export { setCurrency, fetchData, addCoin, changeCoinAmount, removeCoin, addToWatchList, removeFromWatchList, importData, setScanning, setPage, fetchHistory, exportData, loadUserData, saveUserData };
+export {
+  setCurrency,
+  fetchData,
+  addCoin,
+  changeCoinAmount,
+  removeCoin,
+  addToWatchList,
+  removeFromWatchList,
+  importData,
+  setScanning,
+  setPage,
+  fetchHistory,
+  exportData,
+  loadUserData,
+  saveUserData
+};
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   currencies: ["AUD", "BRL", "CAD", "CHF", "CNY", "EUR", "GBP", "HKD", "IDR", "INR", "JPY", "KRW", "MXN", "RUB", "USD"],
-  //api_base: '/api/',
-  api_base: 'http://localhost:4000/api/'
+  api_base: '/api/',
+  //api_base: 'http://localhost:4000/api/'
 }
