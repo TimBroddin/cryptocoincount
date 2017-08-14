51f8cb3a8daac61861374689e34fea8ff5bbca3f
Tim Broddin <tim@wannabes.be>
2017-08-14 15:29:57 +0200

    add authentication

---
+++ server/routes/api/ticker.js
@@ -0,0 +1,24 @@
+const request = require('request-promise');
+
+const ticker = (cache) => (req, res) => {
+  const { convert } = req.query;
+
+  cache.get(`ticker-${convert}`, (err, value) => {
+    if (value) {
+      res.send(value);
+    } else {
+      request
+        .get(
+          `https://api.coinmarketcap.com/v1/ticker/?convert=${convert
+            ? convert.toUpperCase()
+            : ""}`
+        )
+        .then(json => {
+          cache.set(`ticker-${convert}`, json);
+          res.send(json);
+        });
+    }
+  });
+}
+
+module.exports = { ticker };
