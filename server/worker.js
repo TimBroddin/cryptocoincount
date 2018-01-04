35c0e85186f867ae9d11e6b73c0c332f5080355e
Tim Broddin <tim.broddin@vrt.be>
2018-01-04 16:52:58 +0100

    keep less coinhistories

--- server/worker.js
+++ server/worker.js
@@ -1,44 +1,52 @@
-const db = require('./lib/db');
-const { getExchangeRates, getCoins, getLiveCoins, cleanLiveCoins, cleanSync } = require('./lib/populate');
+const db = require("./lib/db");
+const {
+  getExchangeRates,
+  getCoins,
+  getLiveCoins,
+  cleanLiveCoins,
+  cleanSync
+} = require("./lib/populate");
 
 db().then(({ CoinHistory, ExchangeRates, Sync }) => {
   function updateData() {
-    getExchangeRates(ExchangeRates).then(() => {
-      console.log('Updated exchange rates');
-    }).catch((err) => {
-      console.log('Error updating exchange rates');
-    });
+    getExchangeRates(ExchangeRates)
+      .then(() => {
+        console.log("Updated exchange rates");
+      })
+      .catch(err => {
+        console.log("Error updating exchange rates");
+      });
 
-    getCoins(CoinHistory).then(() => {
-      console.log('Updated coins');
-    }).catch((err) => {
-      console.log('Error updating coins');
-    });
+    getCoins(CoinHistory)
+      .then(() => {
+        console.log("Updated coins");
+      })
+      .catch(err => {
+        console.log("Error updating coins");
+      });
 
     cleanLiveCoins(CoinHistory);
-
   }
 
   function updateLive() {
     getLiveCoins(CoinHistory).then(() => {
-      console.log('Live coins updated');
+      console.log("Live coins updated");
     });
   }
 
   setInterval(() => {
     updateData();
-  }, 1000*60*60*12);
+  }, 1000 * 60 * 60 * 12);
 
   setInterval(() => {
     updateLive();
-  }, 1000*60*30);
+  }, 1000 * 60 * 120);
 
   setInterval(() => {
     cleanSync(Sync);
-  }, 1000*30)
+  }, 1000 * 30);
 
   updateData();
   updateLive();
   cleanSync(Sync);
-
 });
